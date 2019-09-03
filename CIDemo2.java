47d914256f724fa8a087a297987772ff68f5008b
srinivasanchennappan <vasankcs@gmail.com>
2019-09-03 16:12:49 +0530

    version 1

--- CIDemo2.java
+++ CIDemo2.java
@@ -1,4 +1,6 @@
 class CIDemo2{
-public static void main(String[] args)
+public static void main(String[] args){
 System.out.println("Welcome to CI-CD world");
+System.out.println("Modified to demonistrate verison 1 ");
+}
 }
